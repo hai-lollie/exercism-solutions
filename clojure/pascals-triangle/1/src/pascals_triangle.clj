3d4e4dd86da2cb05276d9c6807c0390424b039c4
Lollie <code@lollie.slmail.me>
2026-09-07 21:10:06 -0300

    refactor: name next-row, state adjacency rule directly

--- clojure/pascals-triangle/1/src/pascals_triangle.clj
+++ clojure/pascals-triangle/1/src/pascals_triangle.clj
@@ -1,6 +1,8 @@
 (ns pascals-triangle)
 
-(defn- row [prev-row]
-  (vec (concat [1] (map #(apply + %) (partition 2 1 prev-row)) [1])))
+(defn- next-row [prev-row]
+  (vec (concat [1]
+               (map (fn [[a b]] (+ a b)) (partition 2 1 prev-row))
+               [1])))
 
-(def triangle (iterate row [1]))
+(def triangle (iterate next-row [1]))
